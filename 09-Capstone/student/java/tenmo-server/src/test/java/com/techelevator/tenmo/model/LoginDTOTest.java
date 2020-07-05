f358974220b073880aae43d11e6fba7f646ec785
Yichen Zhang <zhangyichen9@gmail.com>
2020-07-05 11:56:21 -0400

    added more coverage to LoginDTO and User tests on server side

--- 09-Capstone/student/java/tenmo-server/src/test/java/com/techelevator/tenmo/model/LoginDTOTest.java
+++ 09-Capstone/student/java/tenmo-server/src/test/java/com/techelevator/tenmo/model/LoginDTOTest.java
@@ -1,12 +1,22 @@
 package com.techelevator.tenmo.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 
-class LoginDTOTest {
+public class LoginDTOTest {
 	
-	LoginDTO test = new LoginDTO();
+	private LoginDTO test;
+	private static final String USERNAME = "GrumpyCat";
+	private static final String PASSWORD = "I don't want no stinkin cheeseburger";
+	
+	@Before
+	public void setup() {
+		test = new LoginDTO();
+		test.setUsername(USERNAME);
+		test.setPassword(PASSWORD);
+	}
 
 	@Test
 	public void username_returns_XxWalter2013xX() {
@@ -20,6 +30,12 @@ class LoginDTOTest {
 		String password = "iheartsocks123";
 		test.setPassword(password);
 		assertEquals(test.getPassword(), password);
+	}
+	
+	@Test
+	public void to_String_returns_JSON_format() {
+		String expected = "LoginDTO{username='" + USERNAME + "', password='" + PASSWORD + "'}";
+		assertEquals(expected, test.toString());
 	}
 
 }
--- 09-Capstone/student/java/tenmo-server/src/test/java/com/techelevator/tenmo/model/UserTest.java
+++ 09-Capstone/student/java/tenmo-server/src/test/java/com/techelevator/tenmo/model/UserTest.java
@@ -1,21 +1,37 @@
 package com.techelevator.tenmo.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+
 // Server side
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+public class UserTest {
 
-class UserTest {
+	private User test;
+	private static final long ID_NUMBER = (long)(Math.random() * 100);
+	private static final String USERNAME = "Tester";
+	private static final String PASSWORD = "Much secure";
+	private static final String AUTHORITIES = "Such authorities";
+	
+	@Before
+	public void setup() {
+		test = new User();
+	}
 
-	User test = new User();
-
+	
 	@Test
 	public void id_returns_66() {
 		long id = 66;
 		test.setId(id);
-		assertEquals(test.getId(), id);
+		assertEquals((long)id, (long)test.getId());
 	}
+	
 	@Test
 	public void username_returns_vessel() {
 		String username = "vessel";
@@ -23,6 +39,7 @@ class UserTest {
 		assertEquals(test.getUsername(), username);
 		
 	}
+	
 	@Test
 	public void password_returns_suregoodokay() {
 		String password = "suregoodokay";
@@ -30,15 +47,31 @@ class UserTest {
 		assertEquals(test.getPassword(), password);
 		
 	}
+	
 	@Test
 	public void activated_returns_false() {
 		test.setActivated(false);
 		assertFalse(test.isActivated());
 	}
+	
 	@Test
 	public void activiated_returns_true() {
 		test.setActivated(true);
 		assertTrue(test.isActivated());
+	}
+	
+	@Test
+	public void detailed_constructor_works_as_expected() {
+		test.setId(ID_NUMBER);
+		test.setUsername(USERNAME);
+		test.setPassword(PASSWORD);
+		test.setActivated(true);
+		User actual = new User(ID_NUMBER, USERNAME, PASSWORD, AUTHORITIES);
+		assertEquals(test, actual);
+		test.setAuthorities(AUTHORITIES);
+		assertNotEquals(test.hashCode(), actual.hashCode());
+		actual.setAuthorities(AUTHORITIES);
+		assertEquals(test.hashCode(), actual.hashCode());
 	}
 
 }
